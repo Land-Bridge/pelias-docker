de7a3ff249cc2f51fc1044e90c5c935ef4502986
alottaland <alottaland@gmail.com>
2025-08-03 19:31:52 -0700

    adding the csv export script.

--- projects/seattle/build.sh
+++ projects/seattle/build.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 set -x # Enable command tracing for debugging
 
+pelias compose down -v
+
+rm -rf data
+mkdir -p data/csv
+
+# Exports data from land_db as csv to import into pelias
+ogr2ogr \
+  -f CSV data/csv/parcels.csv \
+  PG:"host=localhost port=5432 user=postgres dbname=land_db password=password" \
+  -sql "SELECT \
+    spatial_parcel_point_id AS id, \
+    address AS name, \
+    city AS city, \
+    state AS state, \
+    zip AS postcode, \
+    ST_X(ST_Transform(ST_Centroid(geom), 4326)) AS lon, \
+    ST_Y(ST_Transform(ST_Centroid(geom), 4326)) AS lat, \
+    'custom' AS source, \
+    'address' AS layer \
+  FROM gis.property_analysis_combined" \
+  -lco "SEPARATOR=COMMA"
+
 pelias compose pull
 pelias elastic start
 pelias elastic wait
